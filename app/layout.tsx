7d52c4af735f893956ff379f208e7e6b03a01bf6
ydv-manoj <mojirao770@gmail.com>
2025-04-10 01:10:42 +0530

    font fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Mulish } from 'next/font/google';
 
+const inter = Inter({ subsets: ['latin'] });
+const mulish = Mulish({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'E-commerce Dashboard',
@@ -14,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={mulish.className}>{children}</body>
     </html>
   );
 }
